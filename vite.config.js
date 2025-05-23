68c9a25555547c5f0f553577056d9d5a5b3708f3
Rishav Kumar <rishavkumar8507@gmail.com>
2025-05-23 18:40:22 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"My_Portfolio",
+  base:"/My_Portfolio/",
   build:{
     outDir: "./docs",
   },
